5555f187aaf4a3f006d2907ce756867d57472a5e
dedalusium <dedalusium@outlook.fr>
2015-09-08 15:30:43 +0200

    ajout views ui router

--- ecommerce/client/app/app.js
+++ ecommerce/client/app/app.js
@@ -3,24 +3,50 @@
 // Declare app level module which depends on views, and components
 var myapp = angular.module('ecommerce', ['ui.router']);
 myapp.config(function ($stateProvider, $urlRouterProvider) {
-  //
-  // For any unmatched url, redirect to /
+
+  // For any unmatched url, redirect to /index
   $urlRouterProvider.otherwise("/index");
-  //
-  // Now set up the states
-  $stateProvider
-    .state('index', {
-      url: "/index",
-      templateUrl: "main.html",
-      controller: function ($scope) {
-        $scope.articleList = [
-          {name: "Article title", description: "This is a description"},
-          {name: "", description: ""},
-          {name: "", description: ""}
-        ];
-        $scope.shelfList = {};
+
+  $stateProvider.state({
+    name: 'index',
+    url: "/index",
+    views: {
+      '': {templateUrl: 'html/views/main.html'},
+      'header@index': {
+        templateUrl: "html/views/header.html",
+        controller: function ($scope) {
+
+        }
+      },
+      'leftSidebar@index': {
+        templateUrl: "html/views/leftSidebar.html",
+        controller: function ($scope) {
+          $scope.shelfList = {};
+        }
+      },
+      'content@index': {
+        templateUrl: "html/views/content.html",
+        controller: function ($scope) {
+          $scope.articleList = [
+            {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
+          ];
+        }
+      },
+      'rightSidebar@index': {
+        templateUrl: "html/views/rightSidebar.html",
+        controller: function ($scope) {
+
+        }
+      },
+      'footer@index': {
+        templateUrl: "html/views/footer.html",
+        controller: function ($scope) {
+
+        }
       }
-    })
+    }
+  });
+
 });
 
 myapp.run(function ($rootScope) {
